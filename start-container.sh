84a7319680f84c8dae3f0a8aaea12f315c3facc5
Mustafa <mustafa-sabah1999@ymail.com>
2026-07-03 18:57:56 +0300

    final touch

--- start-container.sh
+++ start-container.sh
@@ -5,11 +5,12 @@ mysqld_safe --skip-log-error &
 
 # 2. Wait until MySQL responds
 echo "Waiting for MySQL database server to spin up..."
+
 until mysqladmin ping -h"localhost" --silent; do
     sleep 1
 done
 
-# 3. Secure root account and configure schema settings
+#  Secure root account and configure schema settings
 mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123'; FLUSH PRIVILEGES;"
 # 4. Read your local .sql files directly into the active container database engine
 if [ -d "./dataBase" ]; then
@@ -17,6 +18,6 @@ if [ -d "./dataBase" ]; then
     cat ./dataBase/*.sql | mysql -u root -p123
 fi
 
-# 5. Keep the container open with an interactive terminal bash shell
+#  Keep the container open with  terminal bash shell
 echo "Database imported successfully! Dropping into terminal..."
 exec bash
